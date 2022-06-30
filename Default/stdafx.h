1cb37321ee5f3aed5c286a65a3fe16fcb5c2b498
WANTWON <106663427+WANTWON@users.noreply.github.com>
2022-06-30 17:01:33 +0900

    Setting

--- Default/stdafx.h
+++ Default/stdafx.h
@@ -29,9 +29,8 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 
-/*
-#pragma comment(lib, "d3d9.lib")
-#pragma comment(lib, "d3dx9.lib")*/
+//#pragma comment(lib, "d3d9.lib")
+//#pragma comment(lib, "d3dx9.lib")
 
 #ifdef _DEBUG
 
