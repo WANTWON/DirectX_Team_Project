38d8b25b6675759ca1c88592993c19f1cefcb6c2
WANTWON <106663427+WANTWON@users.noreply.github.com>
2022-07-01 17:35:02 +0900

    First Commit

---
+++ Default/Player4.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Obj.h"
+class CPlayer4 :
+	public CObj
+{
+public:
+	CPlayer4();
+	virtual ~CPlayer4();
+
+public:
+	virtual void Initialize(void) override;
+	virtual int  Update(void) override;
+	virtual void Late_Update(void) override;
+	virtual void Render(HDC hDC) override;
+	virtual void Release(void) override;
+
+private:
+	void		Key_Input(void);
+};
+
--- Default/Stage4.cpp
+++ Default/Stage4.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 #include "Stage4.h"
-
+#include "KeyMgr.h"
+#include "SceneMgr.h"
+#include "AbstractFactory.h"
+#include "Player4.h"
 
 CStage4::CStage4()
 {
@@ -9,25 +12,36 @@ CStage4::CStage4()
 
 CStage4::~CStage4()
 {
+	Release();
 }
 
 void CStage4::Initialize(void)
 {
+	
+	CObjMgr::Get_Instance()->Add_Object(OBJ_PLAYER, CAbstractFactory<CPlayer4>::Create(400, 300));
+
 }
 
 int CStage4::Update(void)
 {
+	CObjMgr::Get_Instance()->Update();
+
 	return 0;
 }
 
 void CStage4::Late_Update(void)
 {
+	CObjMgr::Get_Instance()->Late_Update();
 }
 
 void CStage4::Release(void)
 {
+
 }
 
 void CStage4::Render(HDC hDC)
 {
+	Rectangle(hDC, 0, 0, WINCX, WINCY);
+
+	CObjMgr::Get_Instance()->Render(hDC);
 }
